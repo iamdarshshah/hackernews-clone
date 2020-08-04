3618274d3ca9cf2e437ab228e89d335a47f0a454
iamdarshshah <imdarshshah@gmail.com>
2020-08-04 21:27:59 +0530

    done setting up server code by implementing first GraphQL query 🎉

---
+++ hackernews-node/src/index.js
@@ -0,0 +1,22 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+const typeDefs = `
+type Query {
+  info: String!
+}
+`;
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone 🤡`,
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() =>
+  console.log(`Server is running on http://localhost:4000 🚀`)
+);
