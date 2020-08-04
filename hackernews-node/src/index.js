1a0a47e38e58783303952427057711fa6782cd2c
iamdarshshah <imdarshshah@gmail.com>
2020-08-04 22:40:42 +0530

    👶 A simple Mutation - added Post type mutation to the schema

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,18 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
-
 let links = [
   {
     id: "link-0",
@@ -21,20 +8,27 @@ let links = [
   },
 ];
 
+let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
   },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers,
 });
 
