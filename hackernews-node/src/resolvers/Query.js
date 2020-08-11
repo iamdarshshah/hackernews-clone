3e2acd6a1c0aad040039b76656a005bc8639ee96
iamdarshshah <imdarshshah@gmail.com>
2020-08-11 22:23:56 +0530

    🔖 added Pagination functionality

--- hackernews-node/src/resolvers/Query.js
+++ hackernews-node/src/resolvers/Query.js
@@ -10,6 +10,8 @@ async function feed(parent, args, context, info) {
 
   const links = await context.prisma.link.findMany({
     where,
+    skip: args.skip,
+    take: args.take,
   });
 
   return links;
